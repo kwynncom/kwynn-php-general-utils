4897c3e6329802df21c601e8eee56c827fa8d244
Kwynn <buessresume@gmail.com>
2020-06-20 16:23:49 -0400

    added notes on how isKwGoo() works

--- isKwGoo.php
+++ isKwGoo.php
@@ -1,4 +1,4 @@
-<?php 
+<?php  // see notes at bottom
 
 require_once('/opt/kwynn/kwutils.php');
 
@@ -18,7 +18,7 @@ function isGoo2($rin) {
 
 function isKwGoo() {
 try {
-    $sid = startSSLSession(); // Kwynn 2020/06/06 10:42pm
+    $sid = startSSLSession();
     $hsid = hash('sha256', $sid);
     $dao = new kwmoncli();
     $scoll = $dao->selectCollection('qemail', 'sessions');
@@ -37,12 +37,15 @@ try {
 return false;
 
 }
-
-/* HISTORY
- * // Kwynn 2020/06/06 10:42pm - don't require sessions.php and use newer startSSLSession()
- *  // 09/07 9:19pm - getting rid of open hash
-// 09/03 12:33AM added file_put 
-// 08/30 3:23pm - safer hash of hash
-// 2019/08/26 4:22pm - probably moving to /opt/kwynn
+/* This interacts with my email checker at https://github.com/kwynncom/positive-gmail-check
+ * The email checker uses Google OAUTH / OAUTH2 to correlate a session to an email address.  So isKwGoo() is "Does this session belong to Kwynn's 
+ * email (GMail) address as confirmed by Google OAUTH?" or "is Kwynn? (as confirmed by Google)" 
  * 
+ * The "rand" is not for security.  It was just a unique ID for searching and otherwise keeping track.  Upon further thought, I'm not sure there is a 
+ * point to it.  "myr" meaning the PHP 'r' date readout, or something close.  It just means a human-readable, complete date of when I created that 
+ * hash entry.
+ * 
+ * HISTORY
+ * 
+ * History up through 2020/06/06 is now in GitHub, so I will erase most and then all of this.
  */
