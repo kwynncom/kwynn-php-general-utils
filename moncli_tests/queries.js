6f4843b5508e3079aeced9704171352bacee5799
kwynncom <buessresume@gmail.com>
2021-12-31 21:56:01 -0500

    finished moncli tests for now

--- moncli_tests/queries.js
+++ moncli_tests/queries.js
@@ -1,2 +1,15 @@
 // QID-insert-10
-printjson(db.getCollection('kwcliTest10').insert([{'_id' : 1}, {'_id' : 2}, {'_id' : 3}]));
+printjson(db.getCollection('kwcliTest10').insert([
+    {'_id' : 1, 'n' : 1}, 
+    {'_id' : 2, 'n' : 2}, 
+    {'_id' : 3, 'n' : 3}]));
+
+// QID-sum-10
+printjson(db.getCollection('kwcliTest10').aggregate(
+        [{ $group : { _id : 'sumName', 'sumn' : {'$sum' : '$n'}  }}   ]).toArray());
+
+// QID-drop-final-coll
+printjson(db.getCollection('kwcliTest10').drop());
+
+// QID-drop-final-db
+printjson(db.dropDatabase());
--- moncli_tests/test10.php
+++ moncli_tests/test10.php
@@ -3,16 +3,28 @@ require_once(__DIR__ . '/../' . 'kwutils.php');
 
 class mongocli_tests {
 	
-	const dbcnm = 'kwcliTest10'; // to use the same collection name, has to agree with queries.js
+	const dbcnm = 'kwcliTest10'; // assuming in queries.js and below that coll name is also this
 	const qfile = __DIR__ . '/queries.js';
 	
 	public static function doit() {
 		self::t10();
 		self::t20();
+
 	}
 	
 	private static function t20() {
 		$r = self::qf('insert-10');
+		kwas($r['nInserted'] === 3, 'bad result t20');
+		$q = "db.getCollection('kwcliTest10').find({}).toArray()";
+		$r = self::q($q);
+		kwas($r && is_array($r) && count($r) === 3, "bad test t20 2 moncli");
+		$r = self::qf('sum-10');
+		kwas(intval(round($r['sumn'])) === 6, 'bad result t20 sum');
+		
+		kwas(self::qf('drop-final-coll') === true, 'coll final drop fail');
+		$rdd = self::qf('drop-final-db');
+		kwas($rdd['ok'] === 1, 'drop final db fail');
+		return;
 		
 	}
 	
@@ -42,8 +54,8 @@ class mongocli_tests {
 		
 		echo("q === $q\n");
 		$r = dbqcl::q(self::dbcnm, $q);		
-		echo("result === $r\n");
-		return print_r($r);
+		echo("result === " . print_r($r) . "\n");
+		return $r;
 	}
 	
 	
