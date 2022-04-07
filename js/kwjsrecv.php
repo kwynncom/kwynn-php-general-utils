10e7fb24af51c74bb19191359f227a7bf7573e6d
kwynncom <buessresume@gmail.com>
2022-04-07 01:20:15 -0400

    fixed kwjssrp key bug

--- js/kwjsrecv.php
+++ js/kwjsrecv.php
@@ -8,7 +8,7 @@ function kwjssrp($k = false) {
 		$a = json_decode($j, 1); kwas($a, 'null form object');
 		unset($a['XDEBUG_SESSION_START']);
 		
-		if ($k && ($v = kwifs($a, $k))) return $v;
+		if ($k)  return kwifs($a, $k);
 		return $a;
 	} catch(Exception $ex) {}
 	
