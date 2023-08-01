64dcfd98962c9769246eba213cfae38532d7e838
kwynncom <buessresume@gmail.com>
2023-08-01 00:42:07 -0400

    test / lock seem to work

--- lockTest.php
+++ lockTest.php
@@ -6,12 +6,12 @@ class testKw_sem_lock extends dao_generic_3 {
 	
 	const dbname = 'locktest2023';
 	
-	private sem_lock $olock;
+	private sem_lock | null $olock;
 	
 	public function __construct() {
 		$this->do10();
 		$this->dbinit();
-		$this->do20();
+		// $this->do20();
 		$this->do30();
 	}
 	
@@ -27,27 +27,31 @@ class testKw_sem_lock extends dao_generic_3 {
 	}
 	
 	private function do30() {
-		$pn = 12;
-		$jmax = 30;
+		$pn = 10;
+		$jmax = 1000;
 		$ps = [];
+		$pid = false;
+		$this->olock = null;
 		for($i=0; $i < $pn; $i++) {
-			$myps = $ps[] = pcntl_fork(); // ASKING FOR DISASTER!!!!!!!!!!!!!!!!!
+			
+			if ($pid !== 0) {
+				$pid = $ps[] = pcntl_fork();
+				if ($pid !== 0) continue;
+			}
+			
 			$this->olock = new sem_lock(__FILE__, 'f');
-			for ($j = 0; $j < $jmax; $j++)
-				$this->insertNext();
+			for ($j = 0; $j < $jmax; $j++) $this->insertNext();
+			if ($pid === 0) return;
 		}
 		
-		if ($myps === 0) return;
-		foreach($ps as $p) {
-			pcntl_waitpid($p, $status);
-		}
+		foreach($ps as $p) { pcntl_waitpid($p, $status); }
 		
 	}
 	
 	private function insertNext() {
 		$this->olock->lock();
 		$a = $this->dcoll->findOne([], ['sort' => ['_id' => -1]]);
-		if (!$a) $i = 1;
+		if (!$a) $i = 0;
 		else $i = $a['_id'];
 		$this->dcoll->insertOne(['_id' => $i + 1, 'pid' => getmypid()], ['kwnoup' => true]);
 		$this->olock->unlock();		
