21c78c9b48905537bf8cbcd2d1370627ea322c33
Kwynn Buess <buessresume@gmail.com>
2026-02-12 19:28:35 -0500

    kwjss - POST-only etc

--- js/kwjsrecv.php
+++ js/kwjsrecv.php
@@ -1,36 +1,87 @@
 <?php
 
-function kwjssrp	($k = false, $ifns = null) {
-    $v10 = kwjssrp2025  ($k,	     $ifns);
-    if ($v10 !== $ifns) return $v10; unset($v10);
+// 2026/01 - I'm pretty sure some of my code assumes false for the if not set.
 
-    $rawBody = file_get_contents('php://input');
-    $a = json_decode($rawBody, true);
-    return $a[$k] ?? $ifns;
-}
+function kwjssrp ($k = false,	$ifns = false)		      { return kwGetHTTPValsCl::kwjssrp($k, $ifns);	    }
+function isrv	 ($k,	$ifns = false, string $allow = 'all') { return kwGetHTTPValsCl::kwjssrp($k, $ifns, $allow); }
 
-// 2026/01 - I'm pretty sure some of my code assumes false for the if not set.  
-function kwjssrp2025($k = false, $ifns = FALSE) {
-    try {
 
-	if      (isset($_REQUEST[$k])) 
-		return     $_REQUEST[$k];
+class kwGetHTTPValsCl {
 
-	kwas(isset($_REQUEST['POSTob']), 'no form object 20 - not set');
+    private static function getRawPOST() {
+	static $s;
 
-	kwas(($j = $_REQUEST['POSTob']), 'no form object - not truthy');
-	$a = json_decode($j, 1); kwas($a, 'null form object');
-	unset($a['XDEBUG_SESSION_START']);
+	if (!isset($s)) { $s = file_get_contents('php://input');		}
 
-	$t15 = kwifs($a, 'dataset', $k);
-	if ($t15) return $t15;
+	return $s;
+    }
 
-	if ($k)  return kwifs($a, $k, ['kwiff' => $ifns]);
+
+    private static function getRawPOSTAsArr() {
+	static $a;
+
+	if (!isset($a)) { 
+	    $rawBody = self::getRawPOST();
+	    $a = json_decode($rawBody, true);	
+	}
+
 	return $a;
-    } catch(Exception $ex) {}
+    }
 
-    return $ifns;
+    private static function fromRawPOST($k, $ifns) {
+	static $a = [];
+	if (!$a) { $a = self::getRawPOSTAsArr(); }
+	if ($k) { return $a[$k] ?? $ifns; }
+	else { return $a; }
+    }
+
+    public static function kwjssrp($k, $ifns, $allow = 'all') {
+	$v10 = self::kwjssrp2025	  ($k, $ifns, $allow);
+	if ($v10 !== $ifns) return $v10; unset($v10);
+	return self::fromRawPOST($k, $ifns);
+
+    }
+  
+    private static function kwjssrp2025($k, $ifns, string $allow) {
+	try {
+
+
+	    $t10 = self::getFromSuperGlobals($k, $allow);
+	    if (isset($t10)) return $t10; unset($t10);
+
+	    $j = self::getFromSuperGlobals('POSTob', $allow);
+	    kwas($j, 'no form object 20 (err # 121625 ) - not truthy');
+
+	    $a = json_decode($j, 1); kwas($a, 'null form object');
+	    unset($a['XDEBUG_SESSION_START']);
+
+	    $t15 = kwifs($a, 'dataset', $k);
+	    if ($t15) return $t15;
+
+	    if ($k)  return kwifs($a, $k, ['kwiff' => $ifns]);
+	    return $a;
+	} catch(Exception $ex) {}
+
+	return $ifns;
+    }
+
+    private static function getFromSuperGlobals($k, string $allowIN) {
+	$allow = strtoupper($allowIN);		unset($allowIN);
+
+	switch($allow) { 
+	    case 'ALL' : $a = $_REQUEST; break;
+	    case 'POST': $a = $_POST   ; break;
+	    case 'GET' : $a = $_GET    ; break;
+	    default: kwas(false, 'un-implemented param to isrv() or related (err # 121645 )'); break;
+	}
+
+	if ($k) { return $a[$k] ?? null; }
+	else    { return $a; }
+    }
+
 }
 
 
-function isrv($k, $ifns = null) { return kwjssrp($k, $ifns); }
+
+
+
